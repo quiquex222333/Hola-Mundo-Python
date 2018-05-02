18477e7c885fb5afb1dcdec44e7937c110266919
quiquexxx222333 <juane222333@gmail.com>
2018-05-02 19:47:57 -0400

    first commit

---
+++ Hola Mundo.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 11 20:45:17 2018
+
+@author: RIVERA
+"""
+
+
+print("Hola Mundo")
+print("Esta es la segunda linea")
+print("Esta es la ultima linea")
+suma = 3 + \
+4
+print(suma)
